965fc1ce71d135cd76143e8d6adcf558f568a878
Faisal <officialfaisal1112@gmail.com>
2023-11-14 22:39:16 +0600

    test isLoggedIn

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import { isLoggedIn } from "@/services/auth.service";
+import { redirect } from "next/navigation";
+
+export default async function SetupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const admin = isLoggedIn();
+
+  if (!admin) {
+    redirect("/signIn");
+  }
+
+  return <>{children}</>;
+}
