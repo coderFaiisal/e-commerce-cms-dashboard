4ef0411cd18817d169e8bc409ba271f77cd211be
Faisal <officialfaisal1112@gmail.com>
2023-11-17 01:04:58 +0600

    complete loading functionality

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -3,6 +3,7 @@
 import { useIsStoreExistQuery } from "@/redux/features/store/storeApi";
 import { getAdminInfo } from "@/services/auth.service";
 import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function DashboardLayout({
   children,
@@ -11,8 +12,17 @@ export default function DashboardLayout({
   children: React.ReactNode;
   params: { storeId: string };
 }) {
+  const [isMounted, setIsMounted] = useState(false);
   const admin = getAdminInfo();
   const { data: store } = useIsStoreExistQuery({});
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
 
   if (!admin) {
     redirect("/signIn");
---
+++ components/ui/loader.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { RingLoader } from "react-spinners";
+
+export const Loader = () => {
+  return <RingLoader color="#36d7b7" size={50} />;
+};
