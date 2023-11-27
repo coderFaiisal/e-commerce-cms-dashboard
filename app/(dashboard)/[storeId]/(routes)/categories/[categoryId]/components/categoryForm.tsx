291003e9956a724488d00b33669529fb82ba4ae8
Faisal <officialfaisal1112@gmail.com>
2023-11-27 21:05:56 +0600

    responsive categories form

--- app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/categoryForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/categoryForm.tsx
@@ -167,7 +167,7 @@ export const CategoryForm: React.FC<CategoryFormProps> = ({
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-8 w-full"
         >
-          <div className="md:grid md:grid-cols-3 gap-8">
+          <div className="grid md:grid-cols-3 gap-4 md:gap-8 ">
             <FormField
               control={form.control}
               name="name"
