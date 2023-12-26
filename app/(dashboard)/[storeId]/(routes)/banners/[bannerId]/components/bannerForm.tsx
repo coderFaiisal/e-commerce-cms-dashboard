c1ff99cf4493485f923c88da7509a05cf23d8182
Faisal <officialfaisal1112@gmail.com>
2023-12-26 19:21:19 +0600

    complete banner create form section

---
+++ app/(dashboard)/[storeId]/(routes)/banners/[bannerId]/components/bannerForm.tsx
@@ -0,0 +1,196 @@
+"use client";
+
+import { useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Trash } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+import { Heading } from "@/components/ui/heading";
+import { AlertModal } from "@/components/modals/alertModal";
+import ImageUpload from "@/components/ui/imageUpload";
+import CustomLoader from "@/components/customLoader";
+import { RingLoader } from "react-spinners";
+import { useCreateBannerMutation, useDeleteBannerMutation, useUpdateBannerMutation } from "@/redux/features/banner/bannerApi";
+
+const formSchema = z.object({
+  label: z.string().min(1),
+  imageURL: z.string().min(1),
+});
+
+type BannerFormValues = z.infer<typeof formSchema>;
+
+interface BannerFormProps {
+  initialData: any;
+}
+
+export const BannerForm: React.FC<BannerFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const id = params.bannerId;
+  const storeId = params.storeId;
+
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const [createBanner] = useCreateBannerMutation();
+  const [updateBanner] = useUpdateBannerMutation();
+  const [deleteBanner] = useDeleteBannerMutation();
+
+  const title = initialData ? "Edit banner" : "Create banner";
+  const description = initialData ? "Edit a banner" : "Add a new banner";
+
+  const action = initialData ? "Save changes" : "Create";
+
+  const form = useForm<BannerFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      label: "",
+      imageURL: "",
+    },
+  });
+
+  const onSubmit = async (data: BannerFormValues) => {
+    setLoading(true);
+
+    if (initialData) {
+      const res: any = await updateBanner({ id, data });
+
+      if (res?.data?._id) {
+        router.push(`/${params?.storeId}/banners`);
+        toast.success("Banner updated successfully");
+      } else if (res?.error) {
+        toast.error(res?.error?.message);
+      }
+    } else {
+      const bannerData = {
+        label: data?.label,
+        imageURL: data?.imageURL,
+        storeId,
+      };
+
+      const res: any = await createBanner(bannerData);
+
+      if (res?.data?._id) {
+        router.push(`/${params.storeId}/banners`);
+        toast.success("Banner created successfully");
+      } else if (res?.error) {
+        toast.error(res?.error?.message);
+      }
+    }
+
+    setLoading(false);
+  };
+
+  const onDelete = async () => {
+    setLoading(true);
+
+    const res: any = await deleteBanner(id);
+
+    if (res?.data?._id) {
+      router.push(`/${params.storeId}/banners`);
+      toast.success("Banner deleted successfully");
+    } else if (res?.error) {
+      toast.error(res?.error?.message);
+    }
+
+    setLoading(false);
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant="destructive"
+            size="sm"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="imageURL"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background image</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    disabled={loading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="md:grid md:grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="label"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Label</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Banner label"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            {loading ? (
+              <>
+                {action}
+                <CustomLoader>
+                  <RingLoader color="#ffffff" size={30} />
+                </CustomLoader>
+              </>
+            ) : (
+              action
+            )}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
